8a6b4db9a770029b3ea2a3b6138c3c52b49a4f1b
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-24 00:13:48 -0600

    Add basic pins collection view

--- Pinsplash/Controllers/ViewController.swift
+++ Pinsplash/Controllers/ViewController.swift
@@ -8,12 +8,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // MARK: - PROPERTIES
+    
+    private var images: [UIImage] = []
+    
+    
+    // MARK: - COMPONENTS
+    
+    private let pinsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.register(PinCollectionViewCell.self, forCellWithReuseIdentifier: "pinCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    
+    // MARK: - LIFECYCLE
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // TODO: fetch images
+        
+        // TODO: replace this
+        for i in 1...8 {
+            images.append(UIImage(named: "totoro-\(i)")!)
+        }
+        pinsCollectionView.reloadData()
+    }
+    
+    
+    // MARK: - SETUP
+    
+    private func setup() {
+        setupSubviews()
+        setupLayout()
+        setupDelegates()
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(pinsCollectionView)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            pinsCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pinsCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pinsCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            pinsCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+    private func setupDelegates() {
+        pinsCollectionView.dataSource = self
+        pinsCollectionView.delegate = self
+    }
 }
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pinCell", for: indexPath) as! PinCollectionViewCell
+        cell.image = images[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let imageSize = images[indexPath.row].size
+        return CGSize(width: view.frame.size.width, height: imageSize.height * view.frame.size.width / imageSize.width)
+    }
+    
+    }
 
---
+++ Pinsplash/Views/PinCollectionViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  PinCollectionViewCell.swift
+//  Pinsplash
+//
+//  Created by Ricardo Montemayor on 23/06/23.
+//
+
+import UIKit
+
+class PinCollectionViewCell: UICollectionViewCell {
+    
+    
+    // MARK: - PROPERTIES
+    
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    
+    // MARK: - COMPONENTS
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    // MARK: - SETUP
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
